0408e271817700c14a3631ca4e5db8a7c8a8655d
Aaron <aarongkendall@gmail.com>
2016-06-14 20:57:32 +0100

    simple endpoint returning post data

--- routes/flights.js
+++ routes/flights.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(req.body)
+  console.log(req.body);
+  res.json({'success': 'yes'});
 });
 
 module.exports = router;
