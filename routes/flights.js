f064338b3fabf18c36118cd45ba6989f92376bde
Aaron <aarongkendall@gmail.com>
2016-06-15 21:21:48 +0100

    backend returning expected dummy data until frontend work finished

--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,45 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+let returnData = {
+  'outboundFlight': {
+    'originAirport': 'London Stansted (STN)',
+    'originId': 1234,
+    'destinationAirport': 'Barcelona (ABC)',
+    'destinationCity': 'Barcelona',
+    'destinationCountry': 'Spain',
+    'carrierName': 'EasyJet'
+  },
+  'inboundFlight': {
+    'originAirport': 'Barcelona (ABC)',
+    'originId': 1234,
+    'destinationAirport': 'London Stansted (STN)',
+    'destinationCity': 'London',
+    'destinationCountry': 'United Kingdom',
+    'carrierName': 'EasyJet'
+  },
+  'price': 78
+}
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  res.json({'success': 'yes'});
+  // request.get(url,
+  //   function(error, response, body) {
+  //     if(!error && response.statusCode == 200) {
+  //       organiseFlightResponse(JSON.parse(body));
+  //     }
+  //   });
+  res.json(returnData);
 });
+
+var organiseFlightResponse = function(data) {
+  let quotesArray = data.Quotes;
+  let placesLookupArray = data.Places;
+  let carriersLookupArray = data.Carriers;
+
+  console.log('quotes: ', quotesArray, 'places: ', placesLookupArray, 'carriers: ', carriersLookupArray);
+};
 
 module.exports = router;
