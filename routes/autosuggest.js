5679c3c2f5b37decb7a782ff4b09c968bcb71801
Aaron <aarongkendall@gmail.com>
2016-07-04 21:37:37 +0100

    post changed to get

--- routes/autosuggest.js
+++ routes/autosuggest.js
@@ -3,8 +3,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.post('/', function(req, res, next) {
-  const url = "http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=" + req.body.query + "&apiKey=aa219678594276332752311136353209"
+router.get('/', function(req, res, next) {
+  const url = "http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=" + req.query.query + "&apiKey=aa219678594276332752311136353209"
   request.get(url,
       function(error, response, body) {
           if(!error && response.statusCode == 200) {
